da5e31821c40a82735ea16ebd96687d6b561be0e
Cerino O. Ligutom III <zeferinix@gmail.com>
2018-03-25 19:26:26 +0800

    Convert stateless components to stateful components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,27 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Person.css';
 
-const person = (props) => {
-    // const rnd = Math.random();
-    // console.log('rnd:', rnd);
-    // if (rnd > 0.7) {
-    //     throw new Error('lol');
-    // }
-
-    return (
-        <div className={classes.Person}>
-            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name} />
+class Person extends Component {
+    render() {
+        return (
+            <div className={classes.Person}>
+            <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+            <p>{this.props.children}</p>
+            <input type="text" onChange={this.props.changed} value={this.props.name} />
         </div>
-    );
-};
+        );
+    }
+}
 
-// class person extends React.Component {
-//     render() {
-//         return <p>My name is {this.props}</p>
-//     }
-// }
-
-export default person;
+export default Person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,22 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Person from './Person/Person';
 
-const persons = (props) => props.persons.map((person, index) => {
-    // return <ErrorBoundary key={person.id}>
-    //   <Person 
-    //   click={() => this.deletePersonHandler(index)}
-    //   name={person.name} 
-    //   age={person.age}
-    //   changed={(event) => this.nameChangedHandler(event, person.id)} /></ErrorBoundary>
+class Persons extends Component {
+    render() {
+        return this.props.persons.map((person, index) => {
+            return <Person
+                key={person.id}
+                click={() => this.props.clicked(index)}
+                name={person.name}
+                age={person.age}
+                changed={(event) => this.props.changed(event, person.id)} />
+        });
+    }
+}
 
-    return <Person
-        key={person.id}
-        click={() => props.clicked(index)}
-        name={person.name}
-        age={person.age}
-        changed={(event) => props.changed(event, person.id)} />
-
-});
-
-export default persons;
+export default Persons;
