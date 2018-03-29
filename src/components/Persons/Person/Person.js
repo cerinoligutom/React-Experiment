b78056c882aeb5825515d6abd3ac81cbb6d6daf7
Cerino O. Ligutom III <zeferinix@gmail.com>
2018-03-29 16:11:34 +0800

    One way to implement HOC

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
-// import classes from './Person.css';
+import classes from './Person.css';
+import WithClass from '../../../hoc/WithClass';
 
 class Person extends Component {
     constructor(props) {
@@ -18,18 +19,18 @@ class Person extends Component {
 
     render() {
         console.log('[Person.js] Inside render()');
-        // return (
-        //     <div className={classes.Person}>
-        //     <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
-        //     <p>{this.props.children}</p>
-        //     <input type="text" onChange={this.props.changed} value={this.props.name} />
-        // </div>
-        // );
-        return [
-            <p key="1" onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>,
-            <p key="2">{this.props.children}</p>,
-            <input key="3" type="text" onChange={this.props.changed} value={this.props.name} />
-        ];
+        return (
+            <WithClass classes={classes.Person}>
+                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name} />
+            </WithClass>
+        );
+        // return [
+        //     <p key="1" onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>,
+        //     <p key="2">{this.props.children}</p>,
+        //     <input key="3" type="text" onChange={this.props.changed} value={this.props.name} />
+        // ];
     }
 }
 
